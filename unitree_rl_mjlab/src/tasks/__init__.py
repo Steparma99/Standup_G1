666597365784f64dc9713e8475e7123858183dc6
Steparma99 <parma243@gmail.com>
2026-06-08 12:59:46 +0200

    Make no getup task import optional

--- unitree_rl_mjlab/src/tasks/__init__.py
+++ unitree_rl_mjlab/src/tasks/__init__.py
@@ -1,5 +1,21 @@
-from mjlab.utils.lab_api.tasks.importer import import_packages
+"""Task registry bootstrap.
 
-_BLACKLIST_PKGS = ["utils", ".mdp"]
+This repository is centered on the custom G1 get-up task. Upstream velocity
+and tracking task trees may reference robot assets that are not vendored here,
+so keep them optional during package import.
+"""
 
-import_packages(__name__, _BLACKLIST_PKGS)
+import importlib
+
+
+def _import_optional(module_name: str) -> None:
+    try:
+        importlib.import_module(module_name, package=__name__)
+    except (ImportError, ModuleNotFoundError):
+        return
+
+
+importlib.import_module(".getup", package=__name__)
+_import_optional(".velocity")
+_import_optional(".tracking")
+
